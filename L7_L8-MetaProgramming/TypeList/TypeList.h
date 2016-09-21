2c443286ad6f578ffec9e5c89e199268b3d31d97
dentych <dennis@tychsen.me>
2016-09-21 19:59:07 +0200

    This sucks :poop:

--- L7_L8-MetaProgramming/TypeList/TypeList.h
+++ L7_L8-MetaProgramming/TypeList/TypeList.h
@@ -1,44 +1,54 @@
 #ifndef TYPELIST_TYPELIST_H
 #define TYPELIST_TYPELIST_H
 
-struct NullType{};
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+
+struct NullType {
+};
 
 template<typename L, typename R>
-struct TypeList
-{
+struct TypeList {
     typedef L First;
     typedef R Rest;
 };
 
 #define TYPELIST1(A) TypeList<A, NullType>
-#define TYPELIST2(A,B) TypeList<A, TypeList<B, NullType>>
-#define TYPELIST3(A,B,C) TypeList<A, TypeList<B, TypeList<C, NullType>>>
-#define TYPELIST4(A,B,C,D) \
+#define TYPELIST2(A, B) TypeList<A, TypeList<B, NullType>>
+#define TYPELIST3(A, B, C) TypeList<A, TypeList<B, TypeList<C, NullType>>>
+#define TYPELIST4(A, B, C, D) \
 TypeList<A, TypeList<B, TypeList<C, TypeList<D, NullType>>>>
 
-
 template<typename T, typename U>
-struct IsSame
-{
+struct IsSame {
     static const bool value = false;
 };
 
 template<typename T>
-struct IsSame<T, T>
-{
+struct IsSame<T, T> {
     static const bool value = true;
 };
 
 template<typename TL, typename F>
-struct Contains
-{
-    static const bool value = IsSame<typename TL::First, F>::value || Contains<typename TL::Rest, F>::value;
+struct Contains {
+    static const bool value = IsSame<typename TL::First, F>::value ||
+                              Contains<typename TL::Rest, F>::value;
 };
 
 template<typename F>
-struct Contains<NullType, F>
-{
+struct Contains<NullType, F> {
     static const bool value = false;
+};
+
+template<typename TL, int P>
+struct AtIndex {
+    typedef typename AtIndex<typename TL::Rest, P - 1>::type type;
+};
+
+template<typename TL>
+struct AtIndex<TL, 0> {
+    typedef typename TL::First type;
 };
 
 #endif //TYPELIST_TYPELIST_H
--- L7_L8-MetaProgramming/TypeList/main.cpp
+++ L7_L8-MetaProgramming/TypeList/main.cpp
@@ -11,11 +11,23 @@ int main() {
     std::cout << "IsSame <int , char >:: value=" << IsSame<int, char>::value << std
     ::endl;
 
+    // Contains
     typedef TYPELIST3(int, char, long) TL;
 /* Must be true */
     std::cout << "Contains <TL , int >:: value =" << Contains<TL, int>::value << std::endl;
 /* Must be false */
     std::cout << "Contains <TL , std :: string >:: value =" << Contains<TL, std::string>::value << std::endl;
 
+
+    // AtIndex
+    typedef TYPELIST3(long, char, int) TL2;
+/* Must be true */
+    std::cout << "IsSame < typename AtIndex <TL , 2>:: type , int >:: value " << IsSame<
+            typename AtIndex<TL2, 2>::type, int>::value << std::endl;
+
+    AtIndex<TL2, 2> myInt;
+/* Must be false */
+    std::cout << "IsSame < typename AtIndex <TL , 2>:: type , char >:: value " << IsSame
+            <typename AtIndex<TL2, 2>::type, char>::value << std::endl;
     return 0;
 }

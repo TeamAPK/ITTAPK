c1be50f49503de9e3bf7876fc7b8b6fb4c984a33
dentych <dennis@tychsen.me>
2016-09-21 16:26:24 +0200

    Completed exercise 3.2.2 L8

--- L7_L8-MetaProgramming/TypeList/TypeList.h
+++ L7_L8-MetaProgramming/TypeList/TypeList.h
@@ -11,6 +11,8 @@ struct TypeList
 };
 
 #define TYPELIST1(A) TypeList<A, NullType>
+#define TYPELIST2(A,B) TypeList<A, TypeList<B, NullType>>
+#define TYPELIST3(A,B,C) TypeList<A, TypeList<B, TypeList<C, NullType>>>
 #define TYPELIST4(A,B,C,D) \
 TypeList<A, TypeList<B, TypeList<C, TypeList<D, NullType>>>>
 
@@ -25,6 +27,18 @@ template<typename T>
 struct IsSame<T, T>
 {
     static const bool value = true;
+};
+
+template<typename TL, typename F>
+struct Contains
+{
+    static const bool value = IsSame<typename TL::First, F>::value || Contains<typename TL::Rest, F>::value;
+};
+
+template<typename F>
+struct Contains<NullType, F>
+{
+    static const bool value = false;
 };
 
 #endif //TYPELIST_TYPELIST_H
--- L7_L8-MetaProgramming/TypeList/main.cpp
+++ L7_L8-MetaProgramming/TypeList/main.cpp
@@ -11,5 +11,11 @@ int main() {
     std::cout << "IsSame <int , char >:: value=" << IsSame<int, char>::value << std
     ::endl;
 
+    typedef TYPELIST3(int, char, long) TL;
+/* Must be true */
+    std::cout << "Contains <TL , int >:: value =" << Contains<TL, int>::value << std::endl;
+/* Must be false */
+    std::cout << "Contains <TL , std :: string >:: value =" << Contains<TL, std::string>::value << std::endl;
+
     return 0;
 }
